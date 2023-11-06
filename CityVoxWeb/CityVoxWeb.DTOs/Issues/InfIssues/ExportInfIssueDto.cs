2968b9faa31e0a3a76d9c8af323d9022fade2b27
AlexHutev <alexhutev777@gmail.com>
2023-11-06 17:57:07 +0200

    Added DTOs for the issues types

--- CityVoxWeb/CityVoxWeb.DTOs/Issues/InfIssues/ExportInfIssueDto.cs
+++ CityVoxWeb/CityVoxWeb.DTOs/Issues/InfIssues/ExportInfIssueDto.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CityVoxWeb.DTOs
+namespace CityVoxWeb.DTOs.Issues.InfIssues
 {
-    internal class Class1
+    internal class ExportInfIssueDto
     {
     }
 }
