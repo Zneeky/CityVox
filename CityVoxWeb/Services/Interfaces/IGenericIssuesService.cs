09157ed448bfda1a777f2bdb522ac4cb38bd8d12
Zneeky <arkanahmedov02@gmail.com>
2023-11-14 15:23:05 +0200

    Added IGenericIssuesService contract for dependency injection for a service

---
+++ CityVoxWeb/Services/Interfaces/IGenericIssuesService.cs
@@ -0,0 +1,14 @@
+﻿namespace Services.Interfaces
+{
+    public interface IGenericIssuesService<CreateTDto, ExportTDto, UpdateTDto>
+    {
+        Task<ExportTDto> CreateAsync(CreateTDto createDto);
+        Task<ExportTDto> GetByIdAsync(string id);
+        Task<ICollection<ExportTDto>> GetByMunicipalityAsync(string municipalityId);
+        Task<ExportTDto> UpdateAsync(string id, UpdateTDto updateDto);
+        Task<bool> DeleteAsync(string id);
+        Task<ICollection<ExportTDto>> GetRequestsAsync(int page, int count);
+        Task<int> GetRequestsCountAsync();
+        Task<ICollection<ExportTDto>> GetByUserIdAsync(string userId);
+    }
+}
