e8716c36a89f23a3f8e97c13537a14e3f67d6661
pen4aka <penkopenev2002@gmail.com>
2023-11-14 19:27:16 +0200

    Added some interfaces
    New interfaces

---
+++ CityVoxWeb/CityVoxWeb.Services/Interfaces/IRefreshTokenService.cs
@@ -0,0 +1,17 @@
+﻿using CityVoxWeb.DTOs.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityVoxWeb.Services.Interfaces
+{
+    public interface IRefreshTokenService
+    {
+        Task<string> CreateRefreshTokenAsync(UserWithIdDto user);
+        Task<string> RefreshTokenAsync(string refreshTokenString);
+        Task RevokeTokenAsync(string token);
+        Task<(bool isValid, string userId)> IsValid(string refreshToken, string JwtToken);
+    }
+}
