6f8f43c9df1c465e1031d2d1e04a5fd20ebbe3a4
pen4aka <penkopenev2002@gmail.com>
2023-11-14 19:42:46 +0200

    StartupHelperExtensions class update
    adding services

--- CityVoxWeb/CityVoxWeb.API/StartupHelperExtensions.cs
+++ CityVoxWeb/CityVoxWeb.API/StartupHelperExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using CityVoxWeb.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace CityVoxWeb.API
 {
@@ -7,6 +8,30 @@ namespace CityVoxWeb.API
     {
         public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
         {
+            builder.Services.AddControllers(configure =>
+            {
+                configure.ReturnHttpNotAcceptable = true;
+
+            })
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                })
+                .AddXmlDataContractSerializerFormatters();
+
+            //CORS Policy configuration
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+            });
+
             // Database connection configuration
             builder.Services.AddDbContext<CityVoxDbContext>(options =>
             {
