be3eee6abefcef226ca5e658ca5291c0726f6483
AlexHutev <alexhutev777@gmail.com>
2023-11-15 16:26:56 +0200

    Added some funcionalities in the Emergency service

--- CityVoxWeb/CityVoxWeb.Services/Issue Services/EmergenciesService.cs
+++ CityVoxWeb/CityVoxWeb.Services/Issue Services/EmergenciesService.cs
@@ -1,5 +1,9 @@
-﻿using CityVoxWeb.DTOs.Issues.Emergencies;
+﻿using AutoMapper;
+using CityVoxWeb.Data;
+using CityVoxWeb.Data.Models.IssueEntities;
+using CityVoxWeb.DTOs.Issues.Emergencies;
 using CityVoxWeb.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +14,48 @@ namespace CityVoxWeb.Services.Issue_Services
 {
     public class EmergenciesService : IGenericIssuesService<CreateEmergencyDto, ExportEmergencyDto, UpdateEmergencyDto>
     {
-        public Task<ExportEmergencyDto> CreateAsync(CreateEmergencyDto createDto)
+
+        private readonly CityVoxDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public EmergenciesService(CityVoxDbContext dbContext, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public async Task<ExportEmergencyDto> CreateAsync(CreateEmergencyDto createEmergencyDto)
+        {
+            try
+            {
+                Emergency emergency = _mapper.Map<Emergency>(createEmergencyDto);
+                await _dbContext.Emergencies.AddAsync(emergency);
+                await _dbContext.SaveChangesAsync();
+
+                ExportEmergencyDto exportEmergencyDto = _mapper.Map<ExportEmergencyDto>(emergency);
+                return exportEmergencyDto;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to create an emergency", ex);
+            }
         }
 
-        public Task<bool> DeleteAsync(string id)
+        public async Task<bool> DeleteAsync(string emergencyId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var emergency = await _dbContext.Emergencies
+                    .FirstOrDefaultAsync(e => e.Id.ToString() == emergencyId) ?? throw new Exception("Invalid id!");
+
+                _dbContext.Emergencies.Remove(emergency);
+                await _dbContext.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The emergency was not deleted", ex);
+            }
         }
 
         public Task<ExportEmergencyDto> GetByIdAsync(string id)
@@ -45,9 +83,26 @@ namespace CityVoxWeb.Services.Issue_Services
             throw new NotImplementedException();
         }
 
-        public Task<ExportEmergencyDto> UpdateAsync(string id, UpdateEmergencyDto updateDto)
+        public async Task<ExportEmergencyDto> UpdateAsync(string emergencyId, UpdateEmergencyDto emergencyDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var emergency = await _dbContext.Emergencies
+                    .Include(e => e.Municipality)
+                    .Include(e => e.User)
+                    .FirstOrDefaultAsync(e => e.Id.ToString() == emergencyId)
+                    ?? throw new Exception("Invalid id!");
+
+                _mapper.Map(emergencyDto, emergency);
+                await _dbContext.SaveChangesAsync();
+
+                var exportEmergencyDto = _mapper.Map<ExportEmergencyDto>(emergency);
+                return exportEmergencyDto;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The operation concluded with an exeption!", ex);
+            }
         }
     }
 }
