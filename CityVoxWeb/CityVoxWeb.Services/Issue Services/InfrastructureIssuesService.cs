231a2f1df8409c3e995f5d364bf61c1a4e768afd
AlexHutev <alexhutev777@gmail.com>
2023-11-15 17:01:48 +0200

    Implemented crate,update,delete funcionalitie for the InfrastructureIssuesService

--- CityVoxWeb/CityVoxWeb.Services/Issue Services/InfrastructureIssuesService.cs
+++ CityVoxWeb/CityVoxWeb.Services/Issue Services/InfrastructureIssuesService.cs
@@ -1,5 +1,9 @@
-﻿using CityVoxWeb.DTOs.Issues.InfIssues;
+﻿using AutoMapper;
+using CityVoxWeb.Data;
+using CityVoxWeb.Data.Models.IssueEntities;
+using CityVoxWeb.DTOs.Issues.InfIssues;
 using CityVoxWeb.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,16 +12,71 @@ using System.Threading.Tasks;
 
 namespace CityVoxWeb.Services.Issue_Services
 {
-    internal class InfrastructureIssuesService : IGenericIssuesService<CreateInfIssueDto, ExportInfIssueDto, UpdateInfIssueDto>
+    public class InfrastructureIssuesService : IGenericIssuesService<CreateInfIssueDto, ExportInfIssueDto, UpdateInfIssueDto>
     {
-        public Task<ExportInfIssueDto> CreateAsync(CreateInfIssueDto createDto)
+
+        private readonly CityVoxDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public InfrastructureIssuesService(CityVoxDbContext dbContext, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public async Task<ExportInfIssueDto> CreateAsync(CreateInfIssueDto createInfIssueDto)
+        {
+            try
+            {
+                InfrastructureIssue infIssue = _mapper.Map<InfrastructureIssue>(createInfIssueDto);
+                await _dbContext.InfrastructureIssues.AddAsync(infIssue);
+                await _dbContext.SaveChangesAsync();
+
+                ExportInfIssueDto exportInfIssueDto = _mapper.Map<ExportInfIssueDto>(infIssue);
+                return exportInfIssueDto;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to create an infrastructure issue", ex);
+            }
         }
 
-        public Task<bool> DeleteAsync(string id)
+        public async Task<ExportInfIssueDto> UpdateAsync(string infIssueId, UpdateInfIssueDto updateInfIssueDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var infIssue = await _dbContext.InfrastructureIssues
+                    .Include(i => i.Municipality)
+                    .Include(i => i.User)
+                    .FirstOrDefaultAsync(i => i.Id.ToString() == infIssueId)
+                    ?? throw new Exception("Invalid id!");
+
+                _mapper.Map(updateInfIssueDto, infIssue);
+                await _dbContext.SaveChangesAsync();
+
+                var exportInfIssueDto = _mapper.Map<ExportInfIssueDto>(infIssue);
+                return exportInfIssueDto;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The operation concluded with an exeption!", ex);
+            }
+        }
+        public async Task<bool> DeleteAsync(string infIssueId)
+        {
+            try
+            {
+                var infIssue = await _dbContext.InfrastructureIssues
+                    .FirstOrDefaultAsync(i => i.Id.ToString() == infIssueId) ?? throw new Exception("Invalid id!");
+
+                _dbContext.InfrastructureIssues.Remove(infIssue);
+                await _dbContext.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The infrastructure issue could not be deleted!", ex);
+            }
         }
 
         public Task<ExportInfIssueDto> GetByIdAsync(string id)
@@ -45,9 +104,6 @@ namespace CityVoxWeb.Services.Issue_Services
             throw new NotImplementedException();
         }
 
-        public Task<ExportInfIssueDto> UpdateAsync(string id, UpdateInfIssueDto updateDto)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
