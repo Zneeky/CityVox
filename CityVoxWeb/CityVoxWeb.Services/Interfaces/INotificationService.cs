b7f6f86bc2a136ff213eb764a2290343c466ca1d
Zneeky <arkanahmedov02@gmail.com>
2023-12-11 18:07:56 +0200

    Added notification controller with endpoints for getting and updating

---
+++ CityVoxWeb/CityVoxWeb.Services/Interfaces/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using CityVoxWeb.Data.Models.IssueEntities;
+using CityVoxWeb.DTOs.Notification;
+
+namespace CityVoxWeb.Services.Interfaces
+{
+    public interface INotificationService
+    {
+        Task ChangeNotificationToReadAsync(string notificationId);
+        Task CreateNotificationForEmergencyAsync(int statusValue, string issueType, Emergency emergency);
+        Task CreateNotificationForInfrastructureIssueAsync(int statusValue, string issueType, InfrastructureIssue infrastructureIssue);
+        Task CreateNotificationForReportAsync(int statusValue, string issueType, Report report);
+        Task<List<ExportNotificationDto>> GetUnreadNotificationsByUserIdAsync(string userId);
+    }
+}
--- CityVoxWeb/CityVoxWeb.Services/User Services/NotificationService.cs
+++ CityVoxWeb/CityVoxWeb.Services/User Services/NotificationService.cs
@@ -5,6 +5,9 @@ using CityVoxWeb.Data.Models.IssueEntities;
 using CityVoxWeb.Data.Models.IssueEntities.Enumerators.Emergency;
 using CityVoxWeb.Data.Models.IssueEntities.Enumerators.InfIssue;
 using CityVoxWeb.Data.Models.IssueEntities.Enumerators.Report;
+using CityVoxWeb.DTOs.Notification;
+using CityVoxWeb.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,7 @@ using System.Threading.Tasks;
 
 namespace CityVoxWeb.Services.User_Services
 {
-    public class NotificationService
+    public class NotificationService : INotificationService
     {
         private readonly CityVoxDbContext _dbContext;
         private readonly IMapper _mapper;
@@ -24,39 +27,17 @@ namespace CityVoxWeb.Services.User_Services
             _mapper = mapper;
         }
 
-        public async Task CreateNotificationForReport(int statusValue, string issueType, Report report)
-        {
-            try
-            {
-                string status = GetStatusText(statusValue,issueType);
-                Notification notificationToCreate = new Notification
-                {
-                    UserId = report.UserId,
-                    TimeSent = DateTime.UtcNow,
-                    IsRead = false,
-                    Content = $"Your report about \"{report.Title}\" was {status}, report, {report.Id}",
-                };
-
-                await _dbContext.Notifications.AddAsync(notificationToCreate);
-                await _dbContext.SaveChangesAsync();
-            }
-            catch(Exception ex) 
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        public async Task CreateNotificationForEmergency(int statusValue, string issueType, Emergency emergency)
+        public async Task CreateNotificationForReportAsync(int statusValue, string issueType, Report report)
         {
             try
             {
                 string status = GetStatusText(statusValue, issueType);
                 Notification notificationToCreate = new Notification
                 {
-                    UserId = emergency.UserId,
+                    UserId = report.UserId,
                     TimeSent = DateTime.UtcNow,
                     IsRead = false,
-                    Content = $"Your emergency about \"{emergency.Title}\" was {status}, emergency, {emergency.Id}",
+                    Content = $"Your report submission about \"{report.Title}\" was {status}, report, {report.Id}",
                 };
 
                 await _dbContext.Notifications.AddAsync(notificationToCreate);
@@ -68,7 +49,29 @@ namespace CityVoxWeb.Services.User_Services
             }
         }
 
-        public async Task CreateNotificationForInfrastructureIssue(int statusValue, string issueType, InfrastructureIssue infrastructureIssue)
+        public async Task CreateNotificationForEmergencyAsync(int statusValue, string issueType, Emergency emergency)
+        {
+            try
+            {
+                string status = GetStatusText(statusValue, issueType);
+                Notification notificationToCreate = new Notification
+                {
+                    UserId = emergency.UserId,
+                    TimeSent = DateTime.UtcNow,
+                    IsRead = false,
+                    Content = $"Your emergency submission about \"{emergency.Title}\" was {status}, emergency, {emergency.Id}",
+                };
+
+                await _dbContext.Notifications.AddAsync(notificationToCreate);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task CreateNotificationForInfrastructureIssueAsync(int statusValue, string issueType, InfrastructureIssue infrastructureIssue)
         {
             try
             {
@@ -78,7 +81,7 @@ namespace CityVoxWeb.Services.User_Services
                     UserId = infrastructureIssue.UserId,
                     TimeSent = DateTime.UtcNow,
                     IsRead = false,
-                    Content = $"Your infrastructureIssue about \"{infrastructureIssue.Title}\" was {status},infIssue, {infrastructureIssue.Id}",
+                    Content = $"Your Infrastructure issue submission about \"{infrastructureIssue.Title}\" was {status},infIssue, {infrastructureIssue.Id}",
                 };
 
                 await _dbContext.Notifications.AddAsync(notificationToCreate);
@@ -99,6 +102,48 @@ namespace CityVoxWeb.Services.User_Services
                 "infIssue" => ((InfrastructureIssueStatus)statusValue).ToString(),
                 _ => throw new Exception("Not accepted issue type")
             };
+        }
+
+        public async Task<List<ExportNotificationDto>> GetUnreadNotificationsByUserIdAsync(string userId)
+        {
+            try
+            {
+                var notifications = await _dbContext.Notifications
+                    .Where(n => n.UserId.ToString() == userId && n.IsRead == false)
+                    .ToListAsync();
+
+                var notificationsDtos = _mapper.Map<List<ExportNotificationDto>>(notifications);
+                return notificationsDtos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task ChangeNotificationToReadAsync(string notificationId)
+        {
+            try
+            {
+                var notification = _dbContext.Notifications
+                .Where(n => n.Id.ToString() == notificationId)
+                .FirstOrDefault();
+
+                if (notification != null)
+                {
+                    notification.IsRead = true;
+                    await _dbContext.SaveChangesAsync();
+                }
+                else
+                {
+                    throw new Exception("Notification with the given id vas not found!");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+
         }
     }
 }
